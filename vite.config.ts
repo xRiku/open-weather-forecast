cf4716f237fa30b690523418f6f1047948fbf67a
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2024-05-19 11:41:15 -0300

    :wrench: add conditional base

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/open-weather-forecast/' : '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
   },
-  base: '/open-weather-forecast/',
-})
+}))
